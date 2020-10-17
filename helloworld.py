f371111b709eb023f83050d5afdd0dfac8778c79
ehlacy01 <70302106+ehlacy01@users.noreply.github.com>
2020-10-17 16:07:16 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,13 @@
+def split(word): 
+    return [char for char in word]
 
-
+def getProbability(word):
+    wordList = split(word)
+    ansList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    charList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for n in charList:
+        ansList[n] = (wordList.count(charList[n]))/len(word)
+    return ansList
 list1 = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 import string
 values = dict()
