7d2265a118709242fb4a76f65f56388a8c527a9d
comv2stealth <68664387+comv2stealth@users.noreply.github.com>
2020-10-17 15:37:36 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,10 @@
-list1 = []
+
+
+list1 = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
 import string
 values = dict()
 for index, letter in enumerate(string.ascii_lowercase):
-   values[letter] = index + 1
-for index, letter in enumerate(string.ascii_uppercase):
-    values[letter] = index + 27;
+   values[letter] = list1.count(letter) + list1.count(letter.upper())
 for n in values:
-    print(n)
-    
+    val = 
+    values[n] = float(/len(list1))
